4ecd2ee749286b3becfeccb83b0d1ef68a307fbf
EGNTEC <emerson.gonas@gmail.com>
2019-06-25 19:59:48 -0300

    inserindo cliente

--- PDO/cadCliente.php
+++ PDO/cadCliente.php
@@ -5,9 +5,25 @@ $sql = "SELECT * From TB_CLIENTE";
 $stm = $conn->prepare($sql);
 $stm->execute();
 
- 
-$NOME = $result['NOME_CLIENTE'];
+//Inserindo dados
+if(empty($_POST['nome']) || empty($_POST['dt_nas'])){
+    echo  "Preencha todos os campos!";
+}else{
+    $insert = "INSERT INTO
+    TB_CLIENTE(NOME_CLIENTE,DT_NASCIMENTO,CPF,ENDERECO) 
+    Values(:nome,:dt_nas,:cpf,:ende)";
+    $sti = $conn->prepare($insert);
+    $sti->bindParam(':nome',$_POST['nome']);
+    $sti->bindParam(':dt_nas',$_POST['dt_nas']);
+    $sti->bindParam(':cpf',$_POST['cpf']);
+    $sti->bindParam(':ende',$_POST['end']);
 
+    if($sti->execute()){
+        header('Location: cadCliente.php');
+    }else{
+        print_r($sti->errorInfo());
+    }    
+}
 
 ?>
 <!DOCTYPE html>
@@ -20,7 +36,7 @@ $NOME = $result['NOME_CLIENTE'];
 </head>
 <body>
 <h4>Cadastrar Clientes</h4><br>
-    <form action="cadCliente.php" method="get">
+    <form action="cadCliente.php" method="post">
     <label for="">Nome</label><br>
     <input type="text" name="nome"><br>
     <label for="">Data de nascimento</label><br>
@@ -28,7 +44,7 @@ $NOME = $result['NOME_CLIENTE'];
     <label for="">CPF</label><br>
     <input type="text" name="cpf"><br>
     <label for="">Endereco</label><br>
-    <textarea name="end" cols="30" rows="10">
+    <textarea name="end" cols="30" rows="1">
     </textarea><br>
     <input type="submit" value="Cadastrar">    
     </form><br>
@@ -38,13 +54,18 @@ $NOME = $result['NOME_CLIENTE'];
         <thead>
             <th>ID</th>
             <th>NOME</th>
-            <th>NASCIMENto</th>
+            <th>NASCIMENTO</th>
             <th>CPF</th>
             <th>ENDERECO</th>    
         </thead>
-        <?php while($result = $stm->fetch(PDO::FETCH_ASSOC)) { ?>
+        <?php while($result = $stm->fetch(PDO::FETCH_ASSOC)) { 
+        ?>
         <tbody>
-        
+        <td><?php echo $result['ID_CLIENTE']; ?></td>
+        <td><?php echo $result['NOME_CLIENTE']; ?></td>
+        <td><?php echo $result['DT_NASCIMENTO']; ?></td>
+        <td><?php echo $result['CPF']; ?></td>
+        <td><?php echo $result['ENDERECO']; ?></td>
         </tbody>
         <?php } ?>    
     </table>
